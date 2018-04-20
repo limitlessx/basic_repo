7e8c334eca7f5eebe3d685fae52c583445b840f6
limitlessx <x329457973@gmail.com>
2018-04-19 21:43:21 -0700

    simplified version

--- Ninja Gold/server.py
+++ Ninja Gold/server.py
@@ -7,14 +7,16 @@ from datetime import datetime
 app=Flask(__name__)
 app.secret_key="hithere"
 
-def content():
-  session['color']="green"
-  green=session['color']
-  data ="Earned {} golds from {} ! ({})".format(str(session['gold']),session['building'],session['timestamp']),green
+def content(random_n):
+  session['gold']=random_n
+  data ={
+      "msg":"Earned {} golds from {} ! ({})".format(str(session['gold']),session['building'],session['timestamp']),
+      "color":"green"
+  }
   session['total']=session['total']+session['gold']
-  if data not in session['output']:
-      session['output'].insert(0,data)
-#   session['output'].append((data))
+#   if data not in session['output']:
+#       session['output'].insert(0,data)
+  session['output'].append(data)
   
       
 @app.route('/')
@@ -22,64 +24,44 @@ def root():
     
     if "total" not in session:
         session['total']=0
-    if "output" not in session:
         session['output']=[]
     return render_template('index.html')
 
 @app.route('/process_money',methods=['POST'])
 def process_money():
-
     session['timestamp'] = datetime.now().strftime('%Y/%m/%d %I:%M %p') 
-    session['color']=""
     session['building']=request.form['building']
-    session['red']=""
        
     if session['building']=='farm':
-        session['gold']=random.randrange(10, 20)
-        content()
-        session['color']="green"
+        content(random.randrange(10, 20))
+        
     elif session['building']=='cave':
-        session['gold']=random.randrange(5, 10)
-        content()
-        session['color']="green"
-
+        content(random.randrange(5, 10))
+        
     elif session['building']=='house':
-        session['gold']=random.randrange(2, 5)
-        content()
-        session['color']="green"
+        content(random.randrange(2, 5))
+        
     elif session['building']=='casino':
         win_or_lost=random.randrange(0, 2)
         print "++++++++++++++++++++++++",win_or_lost
         if win_or_lost ==1:
-            session['gold']=random.randrange(0, 50)
-            content()
-            session['color']="green"
+            content(random.randrange(0, 50))
         elif win_or_lost ==0:
-            session['color']="red"
-            red=session['color']
-            data="Entered a {} and lost {} golds from ({})!".format(session['building'],str(session['gold']),session['timestamp']),red
+            data={
+                "msg":"Entered a {} and lost {} golds! from ({})".format(session['building'],str(session['gold']),session['timestamp']),
+                "color":"red"
+            }
             session['total']=session['total']-session['gold']
-            if data not in session['output']:
-                 session['output'].insert(0,data)
-            # session['output'].append((data))
-            if session['total'] <=0:
-                return redirect('/reset')
-
-
-    return redirect('/display')
-
-@app.route('/display')
-def display_gold():
-    #print "this line runs 5"
-    return render_template('index.html')
+            # if data not in session['output']:
+            #      session['output'].insert(0,data)
+            session['output'].append((data))
+            # if session['total'] <=0:
+            #     return redirect('/reset')
+    return redirect('/')
 
 @app.route('/reset')
 def restart_game():
     session.clear()
     return redirect('/')
-
-
-
-
 
 app.run(debug=True)
