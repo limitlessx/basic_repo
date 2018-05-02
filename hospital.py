4500abc3d18af282b8143df95a0e9e5f29b7fc30
limitlessx <30301194+limitlessx@users.noreply.github.com>
2018-05-01 22:49:48 -0700

    Update hospital.py

--- hospital.py
+++ hospital.py
@@ -19,26 +19,28 @@ class hospital(object):
   def __init__(self,name):
     self.patients =[]
     self.hospital_name = name
-    self.capacity = 3
-    print "Welcome to", self.hospital_name.title(), "\n"
+    self.capacity = 4
+    print " ----------Welcome to", self.hospital_name.title(), " ----------\n"
     
   def admit(self,new_patient):
     if len(self.patients)>=self.capacity:
-      print 'the hospital is full'
+      print '#######################\nthe hospital is full\n#######################'.title()
     else:
       self.patients.append(new_patient)
       #print "Patient #{} admitted to bed #{}".format(patient.id, patient.bed_num) 
   
   def spaces(self):
     self.left=self.capacity-len(self.patients)
-    print  "currently {} bed(s) are available \n".format(self.left)
+    print  ">>>currently {} bed(s) are available \n".format(self.left)
   
   def discharge(self,num):
     self.patients.pop(num)
   
   def info(self):
+    print "list of patients:"
     for i in self.patients:
        i.display_patient_info()
+    self.spaces()
     
       
 patient1=patient('john wang', 'peatnut')
@@ -46,7 +48,7 @@ patient1=patient('john wang', 'peatnut')
 patient2=patient('wanda beach', 'coconut')
 patient3=patient('jessica huang', 'icecream')
 patient4=patient('trang nyu', 'rice')
-# patient5=patient('kevin low', 'bbq')
+patient5=patient('kevin low', 'bbq')
 
 
 one=hospital("kaiser")
@@ -54,11 +56,11 @@ one.admit(patient1)
 one.admit(patient2)
 one.admit(patient3)
 one.admit(patient4)
-# one.discharge(0)
+one.discharge(0)
 # one.spaces()
-# one.admit(patient5)
+one.admit(patient5)
 # one.spaces()
 
 
 one.info()
-one.spaces()
+#one.spaces()
