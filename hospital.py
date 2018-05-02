6d56358d39ce6976d1193290ce95b050b890f720
limitlessx <30301194+limitlessx@users.noreply.github.com>
2018-05-01 22:40:06 -0700

    Update hospital.py

--- hospital.py
+++ hospital.py
@@ -1,15 +1,18 @@
 class patient(object):
-  bed_num=0
-  id = 0
+  patients_id = 0
+  patients_bed_num=0
   def __init__(self, name, allergies):
-    patient.id = patient.id+1
+    patient.patients_id+=1
+    patient.patients_bed_num+=1
+    self.id = patient.patients_id
     self.name = name
     self.allergies = allergies
-    patient.bed_num = patient.bed_num +1
+    self.bed_num = patient.patients_bed_num
     #self.display_patient_info()
+   
     
   def display_patient_info(self):
-    print "id: {}\nname: {}\nallergies: {}\nbed_num: {}\n".format(patient.id,self.name.title(),self.allergies.title(),patient.bed_num)
+    print "id: {}\nname: {}\nallergies: {}\nbed_num: {}\n".format(self.id,self.name.title(),self.allergies.title(),self.bed_num)
 
     
 class hospital(object):
@@ -24,6 +27,7 @@ class hospital(object):
       print 'the hospital is full'
     else:
       self.patients.append(new_patient)
+      #print "Patient #{} admitted to bed #{}".format(patient.id, patient.bed_num) 
   
   def spaces(self):
     self.left=self.capacity-len(self.patients)
@@ -38,21 +42,23 @@ class hospital(object):
     
       
 patient1=patient('john wang', 'peatnut')
+# patient1.display_patient_info()
 patient2=patient('wanda beach', 'coconut')
 patient3=patient('jessica huang', 'icecream')
 patient4=patient('trang nyu', 'rice')
-#patient5=patient('kevin low', 'bbq')
+# patient5=patient('kevin low', 'bbq')
 
 
 one=hospital("kaiser")
 one.admit(patient1)
 one.admit(patient2)
 one.admit(patient3)
-one.spaces()
 one.admit(patient4)
-one.discharge(0)
-one.spaces()
-#one.admit(patient5)
-#one.spaces()
+# one.discharge(0)
+# one.spaces()
+# one.admit(patient5)
+# one.spaces()
+
 
 one.info()
+one.spaces()
